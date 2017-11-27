b560c1ceb9032c375e28e9cbbca06b7d308a9611
BadmingtonErwan <erwanb208@gmail.com>
2017-11-27 12:11:05 +0100

    Commentaires

--- main.cpp/cartepuce.cpp
+++ main.cpp/cartepuce.cpp
@@ -6,20 +6,20 @@
 #include <string.h>
 #include <Wire.h>
 
-boolean carteDetectee(void){
+boolean carteDetectee(void){    //Fonction qui permet de detecter si la carte est inseree
   boolean detect=false;
-  if((lireDonnee(0x21)&0x01)==0){
-    detect=true;
+  if((lireDonnee(0x21)&0x01)==0){ //On fait un filtre en utilisant le ET logique entre l'adresse et l'ecriture donnee
+    detect=true;                  //pour obtenir un 0 qui doit deteceter la puce
   }
   return detect;
 }
 
 
-int validationCarte(void){
+int validationCarte(void){    //Fonction qui permet de verifier si la carte rentree est correcte
   int resultat = 0, rang = 0;
   char codeValide[5]={'3','4','5','6','\0'};
   char codeCarte[5]={' ',' ',' ',' ','\0'};
-  if(carteDetectee()==true){
+  if(carteDetectee()==true){  //Si une carte est detectee, alors
     ecrireDonnee(0x21,0x06);
     Wire.beginTransmission(0x50);
     Wire.requestFrom(0x50, 5, true);
--- main.cpp/clavier.cpp
+++ main.cpp/clavier.cpp
@@ -6,28 +6,28 @@
 #include <Wire.h>
 
 
-boolean testClavier(int adresse){       //Fonction permettant de detecter s iune touche est appuyee
+boolean testClavier(int adresse){       //Fonction permettant de detecter si une touche est appuyee
   boolean test = false;
-  ecrireDonnee(adresse, 0x0f);          //
+  ecrireDonnee(adresse, 0x0f);          //On ecrit ca pour detecter si une touche a ete appuyee
   delay(20);
-  if(lireDonnee(adresse)!=(0x0f)){
+  if(lireDonnee(adresse)!=(0x0f)){      //Si la touche appuyee qui est detectee, alors la touche appuyee est differente
     test = true;
   }
   return test;
 }
 
-int toucheAppuyee(int adresse){
+int toucheAppuyee(int adresse){         //Fonction permettant de recuperer la valeur pour chaque touche appuyee
   int ligne, colonne, touche;
-  ecrireDonnee(adresse, 0x0f);
+  ecrireDonnee(adresse, 0x0f);          //en mettant les colonnes et les lignes a l'etat haut
   colonne = lireDonnee(adresse);
   ecrireDonnee(adresse, 0xf0);
   ligne = lireDonnee(adresse);
-  touche = colonne|ligne;
-  return touche;
+  touche = colonne|ligne;               //a l'aide d'un masque OU logique qui permet de recuperer 1 seule valeur pour
+  return touche;                        //une touche appuyee
 }
 
-char tradClavier(int valeur){
-  int touche;
+char tradClavier(int valeur){           //Fonction permettant de traduire la touche appuyee
+  int touche;                           //en fonction de la valeur donnee puis en affectant un caractere
   switch(valeur){
     case 238 :  Serial.println("1");
                 touche = '1';
@@ -75,30 +75,31 @@ char tradClavier(int valeur){
 
 
 
-int validationClavier(void){
+int validationClavier(void){            //Fonction qui permet de verifier si le code rentre est bon
   int resultat = 0;
   char codeValide[5]={'1','2','3','4','\0'};
   char codeEntree[5]={' ',' ',' ',' ','\0'};
   int rang;
-  if(testClavier(0x22)==true){
+  if(testClavier(0x22)==true){        //Si une touche est appuyee
     int essais = 0;
     do{
         for(rang=0;rang<4;rang++){
-            while (testClavier(0x22)==false);
-            codeEntree[rang]=tradClavier(toucheAppuyee(0x22));
-            while (testClavier(0x22)==true);
+            while (testClavier(0x22)==false);   //On ne fait rien tant qu'aucune touche n'est appuyee
+            codeEntree[rang]=tradClavier(toucheAppuyee(0x22));  //On traduit la valeur de la touche appuyee
+            while (testClavier(0x22)==true);    //On ne fait rien tant qu'une touchee est appuyee
         }
         essais++;
-        if(strcmp(codeEntree,codeValide)!=0 && essais<3){
-          effacerAfficheur (0x3b);
+        if(strcmp(codeEntree,codeValide)!=0 && essais<3){ //On compare le code entre avec le code valide
+          effacerAfficheur (0x3b);                        //Si cela est faux, alors on affiche ces messages ci dessous
           envoyerMessage(0x3b,MESSAGE5,LIGNE1);
           envoyerMessage(0x3b,MESSAGE6,LIGNE2);
         }
-    }while(strcmp(codeEntree,codeValide)!=0 && essais<3);
-    if(strcmp(codeEntree,codeValide)==0){
+    }
+    while(strcmp(codeEntree,codeValide)!=0 && essais<3);
+    if(strcmp(codeEntree,codeValide)==0){     //Si le code est bon, on passe resultat a 1
       resultat = 1;
     }
-    else{
+    else{                                                 //Si au bout des 3 essais, l'utilisateur n'a toujours pas le bon code on lui demande de quitter la place
       effacerAfficheur (0x3b);
       envoyerMessage(0x3b,MESSAGE9,LIGNE1);
       envoyerMessage(0x3b,MESSAGE8,LIGNE2);
